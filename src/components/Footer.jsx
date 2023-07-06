50a2082426b8abf246d910b6b9931f9111677651
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-06 14:29:15 +0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear(); //otomatik yıl güncelleme
   return (
-    <div>Footer</div>
+    <footer className='footer'>
+      <div className='container'>
+        <nav className='d-flex justify-content-center'>
+          <NavLink to="/" 
+              className={(navData)=> navData.isActive ? "nav-link active" : "nav-link"}
+          >Home</NavLink>
+          <NavLink to="about"
+            className={(navData)=> navData.isActive ? "nav-link active" : "nav-link"}
+          >About</NavLink>
+          <NavLink to="products"
+            className={(navData)=> navData.isActive ? "nav-link active" : "nav-link"}
+          >Products</NavLink>
+        </nav>
+        <div className='copyrights'>
+          &copy;{currentYear} <Link to="/">WebstylePress  </Link>  - All Rights Reserved      
+        </div>
+      </div>
+    </footer>
+
   )
 }
 
