4872392b583662323955c148dfa34482ef9eb2be
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-06 13:39:23 +0300

    About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,35 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <main>
-      <div>
-        
+    <main className="about">
+      <div className="pg-header">
+        <div className="container">
+          <h1>About</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <div className="row">
+          <div className="col-lg-4">
+            <h2>Why Choose Us.</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Asperiores, vitae.
+            </p>
+          </div>
+          <div className="col-lg-4">
+            <h2>What We Do</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, fugiat?</p>
+          </div>
+          <div className="col-lg-4">
+            <h2>Our Mission</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, fugiat?</p>
+          </div>
+        </div>
+
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
